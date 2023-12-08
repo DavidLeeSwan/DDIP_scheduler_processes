5d81725f14b3b9ad3b8d4b6cc5941ef33fc99bf4
DavidLeeSwan <134301805+DavidLeeSwan@users.noreply.github.com>
2023-12-08 16:20:04 -0700

    Working loads to DB and Hubspot

--- schedule_processes/csvDataLoad.py
+++ schedule_processes/csvDataLoad.py
@@ -9,32 +9,86 @@ import pandas as pd
 load_dir = 'initial_load/'
 sandbox = True
 
-companies = f'{load_dir}test_company.csv'
-# branches = f'{load_dir}branches.csv'
-contacts = f'{load_dir}test_contact.csv'
+company_load_path = f'{load_dir}company_test.csv'
+# branch_load_path = f'{load_dir}branches.csv'
+contact_load_path = f'{load_dir}contact_test.csv'
 
-# ----- Loading to database to start ---------
-ddip.load_all(output_dir='initial_load/', company_file=companies)
-# ddip.load_all(output_dir='initial_load/', branch_file=branches) ## Don't have the branch object in hubspot populated
-ddip.load_all(output_dir='initial_load/', contact_file=contacts)
+sandbox_load = True
+days_ago = 1
+output_str = 'initial_load/results/'
 
-# ---- After database load verified load to sandbox -----------
-companies_data = ddip.export_companies(return_list=True)
-ddip.hubspot_api_inserting.update_hubspot_objects(object_type='companies',
-                                                  objects_to_update=companies_data,
-                                                  sandbox=sandbox)
 
-branches_data = ddip.export_branches(return_list=True)
-ddip.hubspot_api_inserting.update_hubspot_objects(object_type='branches',
-                                                  objects_to_update=branches_data,
-                                                  sandbox=sandbox)
+def csvDataLoad(company_file: str = None, branch_file: str = None, contact_file: str = None):
+    # ---------------- Companies ------------------
+    # loads to the data table
+    if company_file is not None:
+        print('Starting Load of:' , company_file)
+        if os.path.exists(company_file):
+            ddip.table_scrub.company_scrub(filepath=company_file, errored_file=False, output_dir=output_str)
+        if os.path.exists(f'{output_str}scrubbed_new_companies.csv'):
+            ddip.newinsert.load_all(company_file=f'{output_str}scrubbed_new_companies.csv', output_dir=output_str)
+        # scrubs other database tables to try and get missing info from errored entries
+        if os.path.exists(f'{output_str}Errored_Companies_{datetime.today().strftime("%Y-%m-%d")}.csv'):
+            ddip.table_scrub.company_scrub(
+                filepath=f'{output_str}Errored_Companies_{datetime.today().strftime("%Y-%m-%d")}.csv',
+                output_dir=output_str)
+        if os.path.exists(f'{output_str}reload_companies.csv'):
+            ddip.newinsert.load_all(company_file=f'{output_str}reload_companies.csv', output_dir=output_str)
 
-contacts_data = ddip.export_contacts(return_list=True)
-ddip.hubspot_api_inserting.update_hubspot_objects(object_type='contacts',
-                                                  objects_to_update=contacts_data,
-                                                  sandbox=sandbox)
+    # ---------------- Branches ------------------
+    # loads to the data table
+    if branch_file is not None:
+        print('Starting Load of:', branch_file)
+        if os.path.exists(branch_file):
+            ddip.table_scrub.branch_scrub(filepath=branch_file, errored_file=False, output_dir=output_str)
+        if os.path.exists(f'{output_str}scrubbed_new_branches.csv'):
+            ddip.newinsert.load_all(branch_file=f'{output_str}scrubbed_new_branches.csv', output_dir=output_str)
+        # scrubs other database tables to try and get missing info from errored entries
+        if os.path.exists(f'{output_str}Errored_Branches_{datetime.today().strftime("%Y-%m-%d")}.csv'):
+            ddip.table_scrub.branch_scrub(
+                filepath=f'{output_str}Errored_Branches_{datetime.today().strftime("%Y-%m-%d")}.csv',
+                output_dir=output_str)
+        if os.path.exists(f'{output_str}reload_branches.csv'):
+            ddip.newinsert.load_all(branch_file=f'{output_str}reload_branches.csv', output_dir=output_str)
 
-# creates the new associations
-ddip.hubspot_api_misc.set_contact_branch_associations()
+    # ---------------- Contacts ------------------
+    # loads to the data table
+    if contact_file is not None:
+        print('Starting Load of:', contact_file)
+        if os.path.exists(contact_file):
+            ddip.table_scrub.contact_scrub(filepath=contact_file, output_dir=output_str)
+            ddip.newinsert.load_all(contact_file=contact_file, output_dir=output_str)
+        if os.path.exists(f'{output_str}scrubbed_new_contacts.csv'):
+            ddip.newinsert.load_all(contact_file=f'{output_str}scrubbed_new_contacts.csv', output_dir=output_str)
+        # scrubs other database tables to try and get missing info from errored entries
+        if os.path.exists(f'{output_str}Errored_Contacts_{datetime.today().strftime("%Y-%m-%d")}.csv'):
+            ddip.table_scrub.contact_scrub(
+                filepath=f'{output_str}Errored_Contacts_{datetime.today().strftime("%Y-%m-%d")}.csv', output_dir=output_str)
+        if os.path.exists(f'{output_str}reload_contacts.csv'):
+            ddip.newinsert.load_all(contact_file=f'{output_str}reload_contacts.csv', output_dir=output_str)
 
-ddip.hubspot_api_misc.set_branch_company_associations()
+    # ---------------- All loads ------------------
+    # get updated table entries and uploads back to hubspot
+    companies = ddip.extract.export_companies(return_list=True)
+    ddip.api_calls.hubspot_api_inserting.update_hubspot_objects(object_type='companies',
+                                                                objects_to_update=companies,
+                                                                sandbox=sandbox_load)
+
+    branches = ddip.extract.export_branches(return_list=True)
+    ddip.api_calls.hubspot_api_inserting.update_hubspot_objects(object_type='branches',
+                                                                objects_to_update=branches,
+                                                                sandbox=sandbox_load)
+
+    contacts = ddip.extract.export_contacts(return_list=True)
+    ddip.api_calls.hubspot_api_inserting.update_hubspot_objects(object_type='contacts',
+                                                                objects_to_update=contacts,
+                                                                sandbox=sandbox_load)
+
+    # creates the new associations
+    ddip.api_calls.hubspot_api_misc.set_contact_branch_associations()
+
+    ddip.api_calls.hubspot_api_misc.set_branch_company_associations()
+
+
+if __name__ == '__main__':
+    csvDataLoad(company_file=company_load_path, contact_file=contact_load_path)
--- schedule_processes/daily_process.py
+++ schedule_processes/daily_process.py
@@ -7,58 +7,77 @@ import DDIP_Library as ddip
 from datetime import datetime
 import os
 
-sandbox = True
+sandbox_get = False
+sandbox_load = True
 days_ago = 1
 output_str = 'daily_run/'
 
 
 def daily_process():
     # pulls from hubspot
-    ddip.api_calls.hubspot_api_extracting.get_day_old_record(record_type='companies', sandbox=sandbox, num_days=days_ago, output_dir=output_str)
-    ddip.api_calls.hubspot_api_extracting.get_day_old_record(record_type='branches', sandbox=sandbox, num_days=days_ago, output_dir=output_str)
-    ddip.api_calls.hubspot_api_extracting.get_day_old_record(record_type='contacts', sandbox=sandbox, num_days=days_ago, output_dir=output_str)
+    # ddip.api_calls.hubspot_api_extracting.get_day_old_record(record_type='companies',
+    #                                                          sandbox=sandbox_get,
+    #                                                          num_days=days_ago,
+    #                                                          output_dir=output_str)
+    # ddip.api_calls.hubspot_api_extracting.get_day_old_record(record_type='branches',
+    #                                                          sandbox=sandbox_get,
+    #                                                          num_days=days_ago,
+    #                                                          output_dir=output_str)
+    # ddip.api_calls.hubspot_api_extracting.get_day_old_record(record_type='contacts',
+    #                                                          sandbox=sandbox_get,
+    #                                                          num_days=days_ago,
+    #                                                          output_dir=output_str)
 
     # ---------------- Companies ------------------
     # loads to the data table
-    ddip.table_scrub.company_scrub(f'{output_str}new_companies.csv', errored_file=False, output_dir=output_str)
-    ddip.newinsert.load_all(company_file=f'{output_str}scrubbed_new_companies.csv', output_dir=output_str)
-    # scrubs other database tables to try and get missing info from errored entries
-    if os.path.exists(f'{output_str}Errored_Companies_{datetime.today().strftime("%Y-%m-%d")}.csv'):
-        ddip.table_scrub.company_scrub(f'{output_str}Errored_Companies_{datetime.today().strftime("%Y-%m-%d")}.csv', output_dir=output_str)
-    if os.path.exists(f'{output_str}reload_companies.csv'):
-        ddip.newinsert.load_all(company_file=f'{output_str}reload_companies.csv', output_dir=output_str)
-
-    # ---------------- Branches ------------------
-    #loads to the data table
-    ddip.table_scrub.branch_scrub(f'{output_str}new_branches.csv', errored_file=False, output_dir=output_str)
-    ddip.newinsert.load_all(branch_file=f'{output_str}scrubbed_new_branches.csv', output_dir=output_str)
-    # scrubs other database tables to try and get missing info from errored entries
-    if os.path.exists(f'{output_str}Errored_Branches_{datetime.today().strftime("%Y-%m-%d")}.csv'):
-        ddip.table_scrub.branch_scrub(f'{output_str}Errored_Branches_{datetime.today().strftime("%Y-%m-%d")}.csv', output_dir=output_str)
-    if os.path.exists(f'{output_str}reload_branches.csv'):
-        ddip.newinsert.load_all(branch_file=f'{output_str}reload_branches.csv', output_dir=output_str)
+    # ddip.table_scrub.company_scrub(filepath=f'{output_str}new_companies.csv', errored_file=False, output_dir=output_str)
+    # if os.path.exists(f'{output_str}scrubbed_new_companies.csv'):
+    #     ddip.newinsert.load_all(company_file=f'{output_str}scrubbed_new_companies.csv', output_dir=output_str)
+    # # scrubs other database tables to try and get missing info from errored entries
+    # if os.path.exists(f'{output_str}Errored_Companies_{datetime.today().strftime("%Y-%m-%d")}.csv'):
+    #     ddip.table_scrub.company_scrub(filepath=f'{output_str}Errored_Companies_{datetime.today().strftime("%Y-%m-%d")}.csv', output_dir=output_str)
+    # if os.path.exists(f'{output_str}reload_companies.csv'):
+    #     ddip.newinsert.load_all(company_file=f'{output_str}reload_companies.csv', output_dir=output_str)
+    #
+    # # ---------------- Branches ------------------
+    # #loads to the data table
+    # ddip.table_scrub.branch_scrub(filepath=f'{output_str}new_branches.csv', errored_file=False, output_dir=output_str)
+    # if os.path.exists(f'{output_str}scrubbed_new_branches.csv'):
+    #     ddip.newinsert.load_all(branch_file=f'{output_str}scrubbed_new_branches.csv', output_dir=output_str)
+    # # scrubs other database tables to try and get missing info from errored entries
+    # if os.path.exists(f'{output_str}Errored_Branches_{datetime.today().strftime("%Y-%m-%d")}.csv'):
+    #     ddip.table_scrub.branch_scrub(filepath=f'{output_str}Errored_Branches_{datetime.today().strftime("%Y-%m-%d")}.csv', output_dir=output_str)
+    # if os.path.exists(f'{output_str}reload_branches.csv'):
+    #     ddip.newinsert.load_all(branch_file=f'{output_str}reload_branches.csv', output_dir=output_str)
 
     # ---------------- Contacts ------------------
     # loads to the data table
-    ddip.table_scrub.contact_scrub(filepath=f'{output_str}new_contacts.csv', output_dir=output_str)
-    ddip.newinsert.load_all(contact_file=f'{output_str}new_contacts.csv', output_dir=output_str)
-    ddip.newinsert.load_all(contact_file=f'{output_str}scrubbed_new_contacts.csv', output_dir=output_str)
-    # scrubs other database tables to try and get missing info from errored entries
-    if os.path.exists(f'{output_str}Errored_Contacts_{datetime.today().strftime("%Y-%m-%d")}.csv'):
-        ddip.table_scrub.contact_scrub(f'{output_str}Errored_Contacts_{datetime.today().strftime("%Y-%m-%d")}.csv', output_dir=output_str)
-    if os.path.exists(f'{output_str}reload_contacts.csv'):
-        ddip.newinsert.load_all(contact_file=f'{output_str}reload_contacts.csv', output_dir=output_str)
+    # ddip.table_scrub.contact_scrub(filepath=f'{output_str}new_contacts.csv', output_dir=output_str)
+    # ddip.newinsert.load_all(contact_file=f'{output_str}new_contacts.csv', output_dir=output_str)
+    # if os.path.exists(f'{output_str}scrubbed_new_contacts.csv'):
+    #     ddip.newinsert.load_all(contact_file=f'{output_str}scrubbed_new_contacts.csv', output_dir=output_str)
+    # # scrubs other database tables to try and get missing info from errored entries
+    # if os.path.exists(f'{output_str}Errored_Contacts_{datetime.today().strftime("%Y-%m-%d")}.csv'):
+    #     ddip.table_scrub.contact_scrub(filepath=f'{output_str}Errored_Contacts_{datetime.today().strftime("%Y-%m-%d")}.csv', output_dir=output_str)
+    # if os.path.exists(f'{output_str}reload_contacts.csv'):
+    #     ddip.newinsert.load_all(contact_file=f'{output_str}reload_contacts.csv', output_dir=output_str)
 
-    # #---------------- All loads ------------------
+    # ---------------- All loads ------------------
     # get updated table entries and uploads back to hubspot
     companies = ddip.extract.export_companies(return_list=True)
-    ddip.api_calls.hubspot_api_inserting.update_hubspot_objects(object_type='companies', objects_to_update=companies)
+    ddip.api_calls.hubspot_api_inserting.update_hubspot_objects(object_type='companies',
+                                                                objects_to_update=companies,
+                                                                sandbox=sandbox_load)
 
     branches = ddip.extract.export_branches(return_list=True)
-    ddip.api_calls.hubspot_api_inserting.update_hubspot_objects(object_type='branches', objects_to_update=branches)
+    ddip.api_calls.hubspot_api_inserting.update_hubspot_objects(object_type='branches',
+                                                                objects_to_update=branches,
+                                                                sandbox=sandbox_load)
 
     contacts = ddip.extract.export_contacts(return_list=True)
-    ddip.api_calls.hubspot_api_inserting.update_hubspot_objects(object_type='contacts', objects_to_update=contacts)
+    ddip.api_calls.hubspot_api_inserting.update_hubspot_objects(object_type='contacts',
+                                                                objects_to_update=contacts,
+                                                                sandbox=sandbox_load)
 
     # creates the new associations
     ddip.api_calls.hubspot_api_misc.set_contact_branch_associations()
